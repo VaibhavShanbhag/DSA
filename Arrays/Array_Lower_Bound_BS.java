bb43767c31b9c427111f051958ee40b33c08a8e6
VaibhavShanbhag <vaibhavshanbhag467@gmail.com>
2021-08-05 15:31:30 +0530

    Array Lower Bound Index

--- Arrays/Array_Lower_Bound_BS.java
+++ Arrays/Array_Lower_Bound_BS.java
@@ -3,7 +3,7 @@ package Arrays;
 public class Array_Lower_Bound_BS {
     public static void main(String[] args) {
         int[] arr = {1,1,2,2,3,3,4,5,6,6,8};
-        int data = 1;
+        int data = 2;
         int ans = -1;      //if element is found at lower bound index
 
         int low = 0;
