350edbaf953829930a0ae5802854b93624733745
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-01-23 17:11:42 +0530

    Recursion Ex1 and Ex2

---
+++ src/Recursion/Example1.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Example1 {
+    public static void main(String[] args) {
+        fun(3);
+    }
+
+    private static void fun(int n) {
+        if (n == 0){
+            return ;
+        }
+
+        fun(n-1);
+        System.out.println(n);
+        fun(n-1);
+    }
+}
