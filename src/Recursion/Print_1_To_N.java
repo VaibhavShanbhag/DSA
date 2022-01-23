65cd3cd93b2e22d4d8dddccdff02634563bff848
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-01-23 18:05:00 +0530

    Print 1 to N

---
+++ src/Recursion/Print_1_To_N.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Print_1_To_N {
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    private static void fun(int n){
+        if(n == 0)
+            return ;
+
+        fun(n-1);
+        System.out.println(n);
+
+    }
+}
